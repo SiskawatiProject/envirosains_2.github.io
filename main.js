49285fe70bfda2e17db846cd56e5c06b11149b3a
SiskawatiProject <iskandarsiskawati@yahoo.com>
2024-04-27 06:15:12 -0800

    new file:   index.html     new file:   main.js     new file:   styles.css message : push ke server

---
+++ main.js
@@ -0,0 +1,63 @@
+// const PI = 3.14
+
+// let ANGKA = 5
+
+// console.log(PI)
+// console.log(ANGKA)
+
+// ANGKA = 7
+
+// console.log(ANGKA)
+
+// let nama = 'Siska'
+
+// console.log(nama)
+
+// let perempuan = 'True'
+
+// console.log(perempuan)
+
+// FUNGSI
+// Function f(x){
+//     5 + x
+// }
+
+// console.log(f(5))
+// console.log(f(2))
+
+// fungsi
+// function f(x){
+//     return 5 + x
+// }
+
+// console.log(f(5))
+// console.log(f(2))
+
+// function f (x,y){
+//     return 10/x + y
+// }
+
+// console.log(f(2,3))
+
+// const pi = 3.14
+
+// let angka = 5
+
+// console.log(pi)
+
+const PI = 3.14
+
+let angka = 5
+
+console.log(PI)
+console.log(angka)
+
+
+function f(x){
+    return 5 + x
+    }
+console.log(f(5))
+console.log(f(2))
+
+
+
